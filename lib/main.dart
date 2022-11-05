063ac8c2cb970c5b93e4959a217c1a9eeddf22b9
Untara Vivi Chahya <vyvy1777@gmail.com>
2022-11-05 18:52:22 +0700

    layout row, column, stack

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,72 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Hello World'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Layout'),
+        ),
+        body: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                height: 100,
+                width: 50,
+                color: Colors.red,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 200,
+                width: 50,
+                color: Colors.amber,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.purple,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.pink,
+                  ),
+                ],
+              ),
+              Stack(
+                children: [
+                  Container(
+                    height: 100,
+                    width: 50,
+                    color: Colors.orange,
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    height: 200,
+                    width: 50,
+                    color: Colors.brown,
+                  ),
+                ],
+              )
+            ]),
+      ),
     );
   }
 }
