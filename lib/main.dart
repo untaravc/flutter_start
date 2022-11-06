df688bf399a6a0707ddec558a6886a499d843df3
Untara Vivi Chahya <vyvy1777@gmail.com>
2022-11-06 15:19:59 +0700

    counter

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,52 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int counter = 1;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text('Home'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text(
+                counter.toString(),
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  child: Text('-'),
+                  onPressed: () {
+                    setState(() {
+                      counter--;
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  child: Text('+'),
+                  onPressed: () {
+                    setState(() {
+                      counter++;
+                    });
+                  },
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
