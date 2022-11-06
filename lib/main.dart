9038d3913f807ce6bfc418eaf9a32611bcf7608e
Untara Vivi Chahya <vyvy1777@gmail.com>
2022-11-06 14:34:59 +0700

    extract listile

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,14 +23,47 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
+
+  var faker = new Faker();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Home'),
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return ChatItem(
+          imageUrl: 'https://picsum.photos/seed/picsum/200/300',
+          subtitle: faker.lorem.sentence(),
+          title: faker.person.name(),
+        );
+      },
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String imageUrl;
+
+  ChatItem({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
       ),
+      title: Text(title),
+      subtitle: Text(subtitle),
+      trailing: Text('10:23 PM'),
+      onTap: () {},
     );
   }
 }
