850ecbb18e7fa9240731fd062e742260ca4974c1
Untara Vivi Chahya <vyvy1777@gmail.com>
2022-11-08 12:01:43 +0700

    dialog

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,31 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text('Home'),
+        child: ElevatedButton(
+            onPressed: () {
+              print('click');
+              showDialog(
+                  context: context,
+                  builder: (contect) {
+                    return AlertDialog(
+                      title: Text('Confirm delete?'),
+                      content: Text('Are you?'),
+                      actions: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(contect).pop('TRUE');
+                            },
+                            child: Text('Yes')),
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.of(contect).pop('FALSE');
+                            },
+                            child: Text('No'))
+                      ],
+                    );
+                  }).then((value) => print(value));
+            },
+            child: Icon(Icons.delete)),
       ),
     );
   }
