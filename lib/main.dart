cad426db19338cf6f6c24d9c5b541dc2a412437b
Untara Vivi Chahya <vyvy1777@gmail.com>
2022-11-08 11:45:06 +0700

    grid view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Main App'),
+          title: Text('Grid View'),
         ),
         body: Home(),
       ),
@@ -22,14 +24,24 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  final List<Container> myList = List.generate(90, (index) {
+    return Container(
+      color: Color.fromARGB(255, Random().nextInt(255), Random().nextInt(255),
+          Random().nextInt(255)),
+    );
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Home'),
+    return GridView(
+      padding: EdgeInsets.all(10),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 10,
+        childAspectRatio: 4 / 3,
       ),
+      children: myList,
     );
   }
 }
