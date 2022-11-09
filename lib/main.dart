8f9e9a9abc8ea6c47153e916bdfa7bb3c0947341
Untara Vivi Chahya <vyvy1777@gmail.com>
2022-11-09 20:06:13 +0700

    product model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/product.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,13 +23,25 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  List<Product> dummyData = List.generate(100, (index) {
+    return Product(
+        title: 'Judul Produk', price: 2000, imageUrl: 'link_for_image_url');
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
-      child: Text('Hello'),
-    ));
+    return GridView.builder(
+        itemCount: dummyData.length,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (context, index) {
+          return Container(
+            child: Column(children: [
+              Text("title:" + dummyData[index].title),
+              Text("title:" + dummyData[index].price.toString()),
+              Text("title:" + dummyData[index].imageUrl),
+            ]),
+          );
+        });
   }
 }
---
+++ lib/models/product.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  late String title;
+  late int price;
+  late String imageUrl;
+  late String description;
+
+  Product({required this.title, required this.price, required this.imageUrl});
+}
